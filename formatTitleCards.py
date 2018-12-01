faf5aa1f44fd72ec49abb1a1b2c2e3cea2643c61
GarrettGularson <31548884+GarrettGularson@users.noreply.github.com>
2018-12-01 12:19:07 -0500

    Update formatTitleCards.py

--- formatTitleCards.py
+++ formatTitleCards.py
@@ -1,7 +1,12 @@
+#Takes the downloaded title cards and maps what the title says to the name of the file
+
 import os
 
+#Path to the file of title cards
 folder_path = "C:\\Users\\Avi\\Documents\\BlairHacks2018\\titleCards"
 pictures = [f for f in os.listdir(folder_path)]
+
+#Things to change about the titles
 swaps = {"%27":"\'","%3F":"?","%26":"&","%C3%A0":"à"}
 prefix = {"SB_":12,"SB2":11,"S09":8,"220_Episodenkarte-":18,
           "a_Episodenkarte":19,"b_Episodenkarte":19,"229a_Doodle":5}
@@ -20,6 +25,7 @@ complete_swaps = {"073EDFE4-CCF2-4E8E-87CB-25B198F92F0A.jpeg":"No_Pictures_Pleas
                   "Vlcsnap-2017-09-27-21h34m05s079.png":"There's_a_Sponge_in_my_soup.png",
                   "Vlcsnap-2017-11-08-21h43m06s.png":"Cuddle_E._Hugs.png"}
 
+#returns the mapping of the writing in the cards to the name of the file
 def trueName(pictures):
 
     mapping = {}
@@ -46,7 +52,7 @@ def trueName(pictures):
 
     return mapping
 
-
+#writes the information in a text file
 things = trueName(pictures)
 file = open("mapping.txt","w+")
 for title in things:
