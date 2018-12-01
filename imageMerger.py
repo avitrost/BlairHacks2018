49561cc502ef5e00c9d1553d3142561f801ff541
root <root@DESKTOP-2AQR0JN.localdomain>
2018-12-01 12:32:42 -0500

    Implement arrayMerge and Image args instead of paths

--- imageMerger.py
+++ imageMerger.py
@@ -5,37 +5,52 @@ Created on Sat Dec 1 2018
 import numpy as np
 from PIL import Image
 
-def Merge(img_list, isVertical):
+def getImages(paths):
+  return [Image.open(i) for i in paths]
+
+def merge(img_list, isVertical):
+  imgs = getImages(img_list)
   if isVertical:
-    return VMerge(img_list)
+    return vMerge(imgs)
   else:
-    return HMerge(img_list)
+    return hMerge(imgs)
 
-def VMerge(img_list): #img_list: string array of image paths
 
-  imgs = [Image.open(i) for i in img_list]
+def vMerge(imgs): #img_list: array of images
+  
+  if len(imgs) == 0:
+    raise ValueError('No images found')
+    return
+  if len(imgs) == 1:
+    return imgs[0]
 
   x,y = sorted([(np.sum(i.size), i.size) for i in imgs])[0][1] #gets size of smallest image as tuple x,y
 
   new = np.vstack((np.asarray(i.resize((x, int(i.size[1] * (x/i.size[0]))))) for i in imgs))    #images have to be of equal size to combine
 
   new = Image.fromarray(new)          #image from vertical array of images
-  new.save('test.png')
   return new
 
-def HMerge(img_list): #img_list: string array of image paths
 
-  imgs = [Image.open(i) for i in img_list]
+def hMerge(imgs): #img_list: array of images
 
   x,y = sorted([(np.sum(i.size), i.size) for i in imgs])[0][1] #gets size of smallest image as tuple x,y
 
   new = np.hstack((np.asarray(i.resize((int(i.size[0] * (y/i.size[1])),y))) for i in imgs))    #images have to be of equal size to combine
 
   new = Image.fromarray(new)          #image from vertical array of images
-  new.save('test.png')
   return new
 
-imgs = ["test1.png","test2.png","test3.png"]
 
-Merge(imgs, True).show()       #open 
+def arrayMerge(imgArray): #merges 2D array of images
+  return vMerge([hMerge(imgArray[0]),hMerge(imgArray[1])])
+  #return vMerge([hMerge(row) for row in imgArray])
+    
+    
 
+#imgs = ["test1.png","test2.png","test4.jpg","test3.png"]
+#merge(imgs, False).show()       #open
+
+nameArray = [["test1.png","test4.jpg"],["test2.png","test3.png"]]
+imgArray = [getImages(row) for row in nameArray]
+arrayMerge(imgArray).show()
